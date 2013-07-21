f582b5d1f5f3eb4844968f3b547028bbcdb6716d
Claudio Natoli <claudio@Claudios-MacBook-Pro.local>
2013-07-21 17:22:54 +1000

    Static class for MavLink utility methods

---
+++ iGCS/Comm/MavLink/Utility/MavLinkUtiility.h
@@ -0,0 +1,18 @@
+//
+//  MavLinkUtiility.h
+//  iGCS
+//
+//  Created by Claudio Natoli on 21/07/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#include "MavLinkTools.h"
+
+@interface MavLinkUtiility : NSObject
+
++ (NSString*) mavModeEnumToString:(enum MAV_MODE)mode;
++ (NSString*) mavStateEnumToString:(enum MAV_STATE)state;
++ (NSString*) mavCustomModeToString:(int)customMode;
+
+@end
--- iGCS/UserInterface/ViewControllers/GCSMapViewController.m
+++ iGCS/UserInterface/ViewControllers/GCSMapViewController.m
@@ -12,6 +12,8 @@
 #import "MainViewController.h"
 #import "GaugeViewCommon.h"
 
+#import "MavLinkUtiility.h"
+
 #import "CommController.h"
 #import "AppDelegate.h"
 
@@ -724,55 +726,6 @@
                       gotoCoordinates.longitude, gotoCoordinates.latitude, gotoAltitude]];    
 }
 
-// FIXME: move the below utilities to some "utils" file
-+ (NSString*) mavModeEnumToString:(enum MAV_MODE)mode {
-    NSString *str = [NSString stringWithFormat:@""];
-    if (mode & MAV_MODE_FLAG_TEST_ENABLED)          str = [str stringByAppendingString:@"Test "];
-    if (mode & MAV_MODE_FLAG_AUTO_ENABLED)          str = [str stringByAppendingString:@"Auto "];
-    if (mode & MAV_MODE_FLAG_GUIDED_ENABLED)        str = [str stringByAppendingString:@"Guided "];
-    if (mode & MAV_MODE_FLAG_STABILIZE_ENABLED)     str = [str stringByAppendingString:@"Stabilize "];
-    if (mode & MAV_MODE_FLAG_HIL_ENABLED)           str = [str stringByAppendingString:@"HIL "];
-    if (mode & MAV_MODE_FLAG_MANUAL_INPUT_ENABLED)  str = [str stringByAppendingString:@"Manual "];
-    if (mode & MAV_MODE_FLAG_CUSTOM_MODE_ENABLED)   str = [str stringByAppendingString:@"Custom "];
-    if (!(mode & MAV_MODE_FLAG_SAFETY_ARMED))       str = [str stringByAppendingString:@"(Disarmed)"];
-    return str;
-}
-
-+ (NSString*) mavStateEnumToString:(enum MAV_STATE)state {
-    switch (state) {
-        case MAV_STATE_UNINIT:      return @"Uninitialized";
-        case MAV_STATE_BOOT:        return @"Boot";
-        case MAV_STATE_CALIBRATING: return @"Calibrating";
-        case MAV_STATE_STANDBY:     return @"Standby";
-        case MAV_STATE_ACTIVE:      return @"Active";
-        case MAV_STATE_CRITICAL:    return @"Critical";
-        case MAV_STATE_EMERGENCY:   return @"Emergency";
-        case MAV_STATE_POWEROFF:    return @"Power Off";
-        case MAV_STATE_ENUM_END:    break;
-    }
-    return [NSString stringWithFormat:@"MAV_STATE (%d)", state];
-}
-
-+ (NSString*) mavCustomModeToString:(int)customMode {
-    switch (customMode) {
-        case MANUAL:        return @"Manual";
-        case CIRCLE:        return @"Circle";
-        case STABILIZE:     return @"Stabilize";
-        case FLY_BY_WIRE_A: return @"FBW_A";
-        case FLY_BY_WIRE_B: return @"FBW_B";
-        case FLY_BY_WIRE_C: return @"FBW_C";
-        case AUTO:          return @"Auto";
-        case RTL:           return @"RTL";
-        case LOITER:        return @"Loiter";
-        case TAKEOFF:       return @"Takeoff";
-        case LAND:          return @"Land";
-        case GUIDED:        return @"Guided";
-        case INITIALISING:  return @"Initialising";
-
-    }
-    return [NSString stringWithFormat:@"CUSTOM_MODE (%d)", customMode];
-}
-
 - (void) handlePacket:(mavlink_message_t*)msg {
     
     // FIXME: try to avoid repeated work here 
@@ -911,9 +864,9 @@
             mavlink_heartbeat_t heartbeat;
             mavlink_msg_heartbeat_decode(msg, &heartbeat);
             
-            [customModeLabel    setText:[GCSMapViewController mavCustomModeToString:  heartbeat.custom_mode]];
-            [baseModeLabel      setText:[GCSMapViewController mavModeEnumToString:    heartbeat.base_mode]];
-            [statusLabel        setText:[GCSMapViewController mavStateEnumToString:   heartbeat.system_status]];
+            [customModeLabel    setText:[MavLinkUtiility mavCustomModeToString:  heartbeat.custom_mode]];
+            [baseModeLabel      setText:[MavLinkUtiility mavModeEnumToString:    heartbeat.base_mode]];
+            [statusLabel        setText:[MavLinkUtiility mavStateEnumToString:   heartbeat.system_status]];
                         
             // Dis/enable the AUTO button
             if ((heartbeat.custom_mode == AUTO) == autoButton.isEnabled) {
