9828f20b67ec40e290b3703c7cf0d06cfd81b64e
Andy Brown <andymoe@gmail.com>
2013-09-03 15:23:48 -0700

    DRYup DateTimeUtils; Add GMT format to log files.
    - added dateStringWithTimeZone: to DateTimeUtils
      that return a dateString in the format
      yyyy-MM-dd-HHmmssZZZZZ where ZZZZZ is for
      RFC 822 GMT format e.g  +0000 or -0800

--- iGCS/Utils/DateTimeUtils.h
+++ iGCS/Utils/DateTimeUtils.h
@@ -10,13 +10,19 @@
 
 @interface DateTimeUtils : NSObject
 
-// Convenience method to return current date and time
-// as a string in the format yyyy-MM-dd-HHmmss in UTC
+// Return current date and time as a string in the format
+// yyyy-MM-dd-HHmmssZ in UTC with the given file extension.
+// This method is appropriate for log files names.
 -(NSString *)dateStringInUTCWithExtension:(NSString *) ext;
 
-// Convenience method to return current date and time
-// as a string in the format yyyy-MM-dd-HHmmss in the apps defaultTimeZone
+// Return current date and time as a string in the format
+// yyyy-MM-dd-HHmmssZ in the apps defaultTimeZone.
+// This method is appropriate for log file names.
 -(NSString *)dateStringInDefaultTimeZoneWithExtension:(NSString *) ext;
+
+// Return the current date and time as a string in the format
+// yyyy-MM-dd-HHmmssZ using the provided time zone
+-(NSString *)dateStringWithTimeZone:(NSTimeZone *) timeZone;
 
 -(NSString *)dateStringForDate:(NSDate *)date
                     withFormat:(NSString *)formatString
--- iGCS/Utils/DateTimeUtils.m
+++ iGCS/Utils/DateTimeUtils.m
@@ -12,20 +12,21 @@
 @implementation DateTimeUtils
 
 -(NSString *)dateStringInUTCWithExtension:(NSString *) ext {
-    NSDate *currentDate = [NSDate date];
-    NSString *name = [self dateStringForDate:currentDate
-                                     withFormat:@"yyyy-MM-dd-HHmmss"
-                                    andTimeZone:[NSTimeZone timeZoneWithName:@"UTC"]];
+    NSString *name = [self dateStringWithTimeZone:[NSTimeZone timeZoneWithName:@"UTC"]];
+                      return [name stringByAppendingPathExtension:ext];
     return [name stringByAppendingPathExtension:ext];
 }
 
 -(NSString *)dateStringInDefaultTimeZoneWithExtension:(NSString *) ext {
-    NSDate *currentDate = [NSDate date];
-    NSString *name = [self dateStringForDate:currentDate
-                                     withFormat:@"yyyy-MM-dd-HHmmss"
-                                    andTimeZone:[NSTimeZone defaultTimeZone]];
-    
+    NSString *name = [self dateStringWithTimeZone:[NSTimeZone defaultTimeZone]];
     return [name stringByAppendingPathExtension:ext];
+}
+
+- (NSString *)dateStringWithTimeZone:(NSTimeZone *) timeZone {
+    // use RFC 822 GMT format so we would get something like "-0800" for defaultTimeZone
+    // and for UTC it would look like this: "+0000"
+    // see http://www.unicode.org/reports/tr35/tr35-25.html#Time_Zone_Fallback
+    return [self dateStringForDate:[NSDate date] withFormat:@"yyyy-MM-dd-HHmmssZZZZZ" andTimeZone:timeZone];
 }
 
 -(NSString *)dateStringForDate:(NSDate *)date
