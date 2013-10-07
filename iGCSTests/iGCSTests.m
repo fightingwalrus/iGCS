370c27e983a1e9bbcce50b6eca98a6d54332e4dc
Claudio Natoli <claudio@Claudios-MacBook-Pro.local>
2013-10-07 14:04:44 +1100

    Simple conversion failure test added

--- iGCSTests/iGCSTests.m
+++ iGCSTests/iGCSTests.m
@@ -38,6 +38,13 @@
     STAssertTrue([demo numWaypoints] == 0, @"Converted mission should not contain any items");
 }
 
+- (void)testWaypointsHolderConversionFailure {
+    // Like the implementation, this is not intended to be comprehensive or exhaustive
+    STAssertNil([WaypointsHolder createFromQGCString:@"ZGC WPL 110"], @"Incorrectly created mission with invalid header");
+    
+    STAssertNil([WaypointsHolder createFromQGCString:@"QGC WPL 110\n0  1	0	16	0.149999999999999994	0	0	0	8.54800000000000004	47.3759999999999977	550"], @"Incorrectly created mission without autocontinue column");
+}
+
 - (void)testWaypointsHolderConversionSingleWaypoint {
     // First lines of example given in from http://qgroundcontrol.org/mavlink/waypoint_protocol
     NSString* demoQGC = @"QGC WPL 110\n0	1	0	16	0.149999999999999994	0	0	0	8.54800000000000004	47.3759999999999977	550	1";
@@ -53,6 +60,7 @@
     STAssertTrue(item.command == MAV_CMD_NAV_WAYPOINT, @"Mission item should be a waypoint");
     STAssertTrue(item.frame   == MAV_FRAME_GLOBAL,     @"Mission item frame should be global");
     STAssertTrue(item.z       == 550,                  @"Mission item height incorrectly read");
+    STAssertTrue(item.autocontinue == 1,               @"Mission item autocontinue incorrectly read");
 }
 
 - (void)testWaypointsHolderDemoMissionConversion
@@ -71,6 +79,5 @@
     // Check that string from converted mission matches original string
     STAssertEqualObjects(demoQGC, [demo2 toOutputFormat], @"Failed to match original QGC after conversion");
 }
-
 
 @end
