8d92cca6d0aa5bcf3a122a2df084c54e9b0f631f
Andy Brown <andymoe@gmail.com>
2013-12-07 15:30:01 -0800

    Return correct autopilot custom mode
    - Take into account custom modes
      based on MAV_AUTOPILOT and MAV_TYPE
      since fixed wing and quad custom
      modes differ. Will need to expand
      to all MAV supported types and
      autopilots down the road.

--- iGCS/Comm/MavLink/Utility/MavLinkUtility.m
+++ iGCS/Comm/MavLink/Utility/MavLinkUtility.m
@@ -10,6 +10,7 @@
 
 @implementation MavLinkUtility
 
+
 NSMutableDictionary *_missionItemMetadata;
 
 + (void) initialize {
@@ -156,24 +157,48 @@ NSMutableDictionary *_missionItemMetadata;
     return [NSString stringWithFormat:@"MAV_STATE (%d)", state];
 }
 
-+ (NSString*) mavCustomModeToString:(int)customMode {
-    switch (customMode) {
-        case MANUAL:        return @"Manual";
-        case CIRCLE:        return @"Circle";
-        case STABILIZE:     return @"Stabilize";
-        case FLY_BY_WIRE_A: return @"FBW_A";
-        case FLY_BY_WIRE_B: return @"FBW_B";
-        case FLY_BY_WIRE_C: return @"FBW_C";
-        case AUTO:          return @"Auto";
-        case RTL:           return @"RTL";
-        case LOITER:        return @"Loiter";
-        case TAKEOFF:       return @"Takeoff";
-        case LAND:          return @"Land";
-        case GUIDED:        return @"Guided";
-        case INITIALISING:  return @"Initialising";
-            
++ (NSString*) mavCustomModeToString:(mavlink_heartbeat_t) heartbeat {
+
+    // ArduPlane Auto Pilot Modes
+    if (heartbeat.autopilot == MAV_AUTOPILOT_ARDUPILOTMEGA && heartbeat.type == MAV_TYPE_FIXED_WING) {
+        switch (heartbeat.custom_mode) {
+            case MANUAL:        return @"Manual";
+            case CIRCLE:        return @"Circle";
+            case STABILIZE:     return @"Stabilize";
+            case FLY_BY_WIRE_A: return @"FBW_A";
+            case FLY_BY_WIRE_B: return @"FBW_B";
+            case FLY_BY_WIRE_C: return @"FBW_C";
+            case AUTO:          return @"Auto";
+            case RTL:           return @"RTL";
+            case LOITER:        return @"Loiter";
+            case TAKEOFF:       return @"Takeoff";
+            case LAND:          return @"Land";
+            case GUIDED:        return @"Guided";
+            case INITIALISING:  return @"Initialising";
+                
+        }
     }
-    return [NSString stringWithFormat:@"CUSTOM_MODE (%d)", customMode];
+
+    // ArduCopter Auto Pilot Modes
+    if (heartbeat.autopilot == MAV_AUTOPILOT_ARDUPILOTMEGA && heartbeat.type == MAV_TYPE_QUADROTOR) {
+        switch (heartbeat.custom_mode) {
+            case Stabilize: return @"Stabilize";
+            case Acro: return @"Acro";
+            case AltHold: return @"AltHold";
+            case Auto: return @"Auto";
+            case Guided: return @"Guided";
+            case Loiter: return @"Loiter";
+            case Rtl: return @"Rtl";
+            case Circle: return @"Circle";
+            case Position: return @"Position";
+            case Land: return @"Land";
+            case OfLoiter: return @"OfLoiter";
+            case Drift: return @"Drift";
+            case Sport: return @"Sport";
+        }
+    }
+
+   return [NSString stringWithFormat:@"CUSTOM_MODE (%d)", heartbeat.custom_mode];
 }
 
 
