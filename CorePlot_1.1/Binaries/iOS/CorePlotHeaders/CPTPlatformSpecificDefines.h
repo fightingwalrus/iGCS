bc3bb441036039b2697bf9ac2b45bc3f47dcf76f
Andy Brown <andymoe@gmail.com>
2014-01-24 23:29:15 -0800

    Add core-plot as a submodule

--- CorePlot_1.1/Binaries/iOS/CorePlotHeaders/CPTPlatformSpecificDefines.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-typedef UIImage CPTNativeImage; ///< Platform-native image format.
-typedef UIEvent CPTNativeEvent; ///< Platform-native OS event.
