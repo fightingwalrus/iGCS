1e6ec789e906df2a100e74a5cf9fa46a33490ab3
Claudio Natoli <claudio@Claudios-MacBook-Pro.local>
2014-03-04 11:34:51 +1100

    Introducing CXAlertView and LiveFrost
     - CXAlertView: e9e5eef1e2c8243bd54d259159a9f41d7937f459
     - LiveFrost: 7a0d9475c42783ff1d62776430c6cb13337afc12

--- ATTRIBUTION.md
+++ ATTRIBUTION.md
@@ -7,7 +7,16 @@ glypish-icons
 - http://glyphish.com/ (Free set published under a Creative Commons Attribution license)
 
 CorePlot
-- as per License.txt file
+- as per https://github.com/core-plot/core-plot/blob/master/License.txt
 
 SWRevealViewController
-- public domain: https://github.com/John-Lluch/SWRevealViewController
+- https://github.com/John-Lluch/SWRevealViewController (public domain)
+
+CXAlertView
+ - https://github.com/ChrisXu1221/CXAlertView (MIT license)
+ 
+LiveFrost
+ - https://github.com/radi/LiveFrost (MIT license)
+
+ 
+
---
+++ LiveFrost/LFDisplayBridge.m
@@ -0,0 +1,79 @@
+//
+// Copyright (c) 2013-2014 Evadne Wu and Nicholas Levin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#import "LFDisplayBridge.h"
+
+void LF_refreshAllSubscribedViewsApplierFunction(const void *value, void *context);
+
+@interface LFDisplayBridge ()
+
+@property (nonatomic, readwrite, assign) CFMutableSetRef subscribedViews;
+@property (nonatomic, readonly, strong) CADisplayLink *displayLink;
+
+@end
+
+void LF_refreshAllSubscribedViewsApplierFunction(const void *value, void *context) {
+	[(__bridge UIView<LFDisplayBridgeTriggering> *)value refresh];
+}
+
+@implementation LFDisplayBridge
+
++ (instancetype) sharedInstance {
+	static dispatch_once_t onceToken;
+	static LFDisplayBridge *object = nil;
+	dispatch_once(&onceToken, ^{
+		object = [self new];
+	});
+	return object;
+}
+
+- (id) init {
+	if (self = [super init]) {
+		_subscribedViews = CFSetCreateMutable(kCFAllocatorDefault, 0, NULL);
+		_displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(handleDisplayLink:)];
+		[_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+	}
+	return self;
+}
+
+- (void) addSubscribedViewsObject:(UIView<LFDisplayBridgeTriggering> *)object {
+	CFSetAddValue(_subscribedViews, (__bridge const void*)object);
+}
+
+- (void) removeSubscribedViewsObject:(UIView<LFDisplayBridgeTriggering> *)object {
+	CFSetRemoveValue(_subscribedViews, (__bridge const void*)object);
+}
+
+- (void) handleDisplayLink:(CADisplayLink *)displayLink {
+	[self refresh];
+}
+
+- (void) dealloc {
+	[_displayLink invalidate];
+	CFRelease(_subscribedViews);
+}
+
+- (void) refresh {
+	CFSetApplyFunction(_subscribedViews, LF_refreshAllSubscribedViewsApplierFunction, NULL);
+}
+
+@end
