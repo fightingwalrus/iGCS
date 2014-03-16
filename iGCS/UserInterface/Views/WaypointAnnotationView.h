978749b566296a73199cf0dc72195fde47ea82d0
Claudio Natoli <cnatoli@users.noreply.github.com>
2014-03-16 14:05:27 +1100

    Add WaypointAnnotationView to project

---
+++ iGCS/UserInterface/Views/WaypointAnnotationView.h
@@ -0,0 +1,13 @@
+//
+//  WaypointAnnotationView.h
+//  iGCS
+//
+//  Created by Claudio Natoli on 16/03/2014.
+//
+//
+
+#import <MapKit/MapKit.h>
+
+@interface WaypointAnnotationView : MKAnnotationView
+
+@end
---
+++ iGCS/UserInterface/Views/WaypointAnnotationView.m
@@ -0,0 +1,32 @@
+//
+//  WaypointAnnotationView.m
+//  iGCS
+//
+//  Created by Claudio Natoli on 16/03/2014.
+//
+//
+
+#import "WaypointAnnotationView.h"
+#import "WaypointAnnotation.h"
+
+#define WAYPOINT_INNER_RADIUS 8
+#define WAYPOINT_OUTER_WIDTH  1
+
+@implementation WaypointAnnotationView
+
+- (void)drawRect:(CGRect)rect
+{
+    CGRect centre = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 0, 0);
+    CGRect outerR = CGRectInset(centre, -WAYPOINT_INNER_RADIUS, -WAYPOINT_INNER_RADIUS);
+    CGRect innerR = CGRectInset(outerR, WAYPOINT_OUTER_WIDTH, WAYPOINT_OUTER_WIDTH);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [UIColor blackColor].CGColor);
+    CGContextFillEllipseInRect(context, outerR);
+    
+    CGContextSetFillColorWithColor(context, [(WaypointAnnotation*)[self annotation] getColor].CGColor);
+    CGContextFillEllipseInRect(context, innerR);
+}
+
+@end
