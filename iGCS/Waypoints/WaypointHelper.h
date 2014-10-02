b73d8dd892c3228c42cf89364515d52d1c42ed18
Claudio Natoli <cnatoli@users.noreply.github.com>
2014-10-02 20:25:05 +1000

    Adopt usual singleton idiom

--- iGCS/Waypoints/WaypointHelper.h
+++ iGCS/Waypoints/WaypointHelper.h
@@ -9,9 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "MavLinkPacketHandler.h"
 
-@interface WaypointHelper : NSObject {
-    NSDictionary *cmdID_to_String;
-}
+@interface WaypointHelper : NSObject 
 
 + (NSString*) commandIDToString:(uint8_t)cmdID;
 + (NSString*) navWaypointToDetailString:(mavlink_mission_item_t)waypoint;
