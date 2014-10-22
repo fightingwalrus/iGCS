7141ea5bc81d3e935980b0520c4e3a764f57f272
David Leskowicz <dave@fightingwalrus.com>
2014-10-22 15:17:34 -0700

    Made it so each button is pined off the previous one.
    Centered, and pin the Emergency button to the bottom.  Also made the emergency button bigger.
    
    Made the buttons change color when they are clicked

--- iGCS/UserInterface/ViewControllers/ArDroneViewController.m
+++ iGCS/UserInterface/ViewControllers/ArDroneViewController.m
@@ -66,62 +66,100 @@
     [self.ftpButton setTitle :@"FTP program to drone" forState:UIControlStateNormal];
     [self.ftpButton addTarget:self action:@selector(ftpProgram) forControlEvents:UIControlEventTouchUpInside];
     [self.ftpButton setTitleColor:[GCSThemeManager sharedInstance].appTintColor forState:UIControlStateNormal];
+    [self.ftpButton setTitleColor:[GCSThemeManager sharedInstance].waypointOtherColor forState:UIControlStateHighlighted];
     [self.view addSubview:self.ftpButton];
-    [self.ftpButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
-    [self.ftpButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:50.0f];
+ 
     
     
     self.telnetButton = [UIButton newAutoLayoutView];
     [self.telnetButton setTitle :@"Execute program on drone" forState:UIControlStateNormal];
     [self.telnetButton addTarget:self action:@selector(telnetProgram) forControlEvents:UIControlEventTouchUpInside];
     [self.telnetButton setTitleColor:[GCSThemeManager sharedInstance].appTintColor forState:UIControlStateNormal];
+    [self.telnetButton setTitleColor:[GCSThemeManager sharedInstance].waypointOtherColor forState:UIControlStateHighlighted];
     [self.view addSubview:self.telnetButton];
-    [self.telnetButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
-    [self.telnetButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:80.0f];
     
     
     self.takeOffButton = [UIButton newAutoLayoutView];
     [self.takeOffButton setTitle :@"Launch Drone" forState:UIControlStateNormal];
     [self.takeOffButton addTarget:self action:@selector(takeoffProgram) forControlEvents:UIControlEventTouchUpInside];
     [self.takeOffButton setTitleColor:[GCSThemeManager sharedInstance].appTintColor forState:UIControlStateNormal];
+    [self.takeOffButton setTitleColor:[GCSThemeManager sharedInstance].waypointOtherColor forState:UIControlStateHighlighted];
     [self.view addSubview:self.takeOffButton];
-    [self.takeOffButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
-    [self.takeOffButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:130.0f];
+
     
     self.landButton = [UIButton newAutoLayoutView];
     [self.landButton setTitle :@"Land Drone" forState:UIControlStateNormal];
     [self.landButton addTarget:self action:@selector(landProgram) forControlEvents:UIControlEventTouchUpInside];
     [self.landButton setTitleColor:[GCSThemeManager sharedInstance].appTintColor forState:UIControlStateNormal];
+    [self.landButton setTitleColor:[GCSThemeManager sharedInstance].waypointOtherColor forState:UIControlStateHighlighted];
     [self.view addSubview:self.landButton];
-    [self.landButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
-    [self.landButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:160.0f];
-    
     
     self.calibrationButton = [UIButton newAutoLayoutView];
     [self.calibrationButton setTitle :@"Calibrate Magnetometer" forState:UIControlStateNormal];
     [self.calibrationButton addTarget:self action:@selector(calibrationProgram) forControlEvents:UIControlEventTouchUpInside];
     [self.calibrationButton setTitleColor:[GCSThemeManager sharedInstance].appTintColor forState:UIControlStateNormal];
+    [self.calibrationButton setTitleColor:[GCSThemeManager sharedInstance].waypointOtherColor forState:UIControlStateHighlighted];
     [self.view addSubview:self.calibrationButton];
-    [self.calibrationButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
-    [self.calibrationButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:190.0f];
-
+ 
     
     self.doAnimationButton = [UIButton newAutoLayoutView];
     [self.doAnimationButton setTitle :@"Do Animation" forState:UIControlStateNormal];
     [self.doAnimationButton addTarget:self action:@selector(doAnimationProgram) forControlEvents:UIControlEventTouchUpInside];
     [self.doAnimationButton setTitleColor:[GCSThemeManager sharedInstance].appTintColor forState:UIControlStateNormal];
+    [self.doAnimationButton setTitleColor:[GCSThemeManager sharedInstance].waypointOtherColor forState:UIControlStateHighlighted];
     [self.view addSubview:self.doAnimationButton];
-    [self.doAnimationButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
-    [self.doAnimationButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:220.0f];
     
     
     self.emergencyButton = [UIButton newAutoLayoutView];
     [self.emergencyButton setTitle :@"Send Emergency Signal" forState:UIControlStateNormal];
+    [self.emergencyButton.titleLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:35.0]];
     [self.emergencyButton addTarget:self action:@selector(emergencyProgram) forControlEvents:UIControlEventTouchUpInside];
     [self.emergencyButton setTitleColor:[GCSThemeManager sharedInstance].appTintColor forState:UIControlStateNormal];
+    [self.emergencyButton setTitleColor:[GCSThemeManager sharedInstance].waypointOtherColor forState:UIControlStateHighlighted];
     [self.view addSubview:self.emergencyButton];
-    [self.emergencyButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
-    [self.emergencyButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:300.0f];
+    [self.emergencyButton autoAlignAxisToSuperviewAxis:ALAxisVertical];
+    [self.emergencyButton autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset:10.0f];
+    
+
+    [self.ftpButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
+    [self.ftpButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:80.0f];
+    
+    NSArray *buttonlViews = @[self.ftpButton, self.telnetButton];
+    
+    UIView *previousLabel = nil;
+    for (UIView *view in buttonlViews) {
+        if (previousLabel) {
+            [view autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:previousLabel withOffset:0.0f];
+            [view autoPinEdge:ALEdgeLeft toEdge:ALEdgeLeft ofView:previousLabel withOffset:0.0f];
+            UILayoutPriority priority = (view == self.ftpButton) ? UILayoutPriorityDefaultHigh + 1 : UILayoutPriorityRequired;
+            [UIView autoSetPriority:priority forConstraints:^{
+                [view autoMatchDimension:ALDimensionHeight toDimension:ALDimensionHeight ofView:previousLabel];
+            }];
+        }
+        previousLabel = view;
+    }
+    
+    
+    
+    [self.takeOffButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:10.0f];
+    [self.takeOffButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:180.0f];
+    
+    NSArray *buttonlViews2 = @[self.takeOffButton, self.landButton, self.calibrationButton, self.doAnimationButton];
+    
+    previousLabel = nil;
+    for (UIView *view in buttonlViews2) {
+        if (previousLabel) {
+            [view autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:previousLabel withOffset:0.0f];
+            [view autoPinEdge:ALEdgeLeft toEdge:ALEdgeLeft ofView:previousLabel withOffset:0.0f];
+            UILayoutPriority priority = (view == self.takeOffButton) ? UILayoutPriorityDefaultHigh + 1 : UILayoutPriorityRequired;
+            [UIView autoSetPriority:priority forConstraints:^{
+                [view autoMatchDimension:ALDimensionHeight toDimension:ALDimensionHeight ofView:previousLabel];
+            }];
+        }
+        previousLabel = view;
+    }
+
+
 
     
 }
