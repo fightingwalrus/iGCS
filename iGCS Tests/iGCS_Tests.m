a655d06ff3fad5ea497f995a7749e07625534196
Claudio Natoli <cnatoli@users.noreply.github.com>
2015-02-04 12:39:04 +1100

    Removed examples

--- iGCS Tests/iGCS_Tests.m
+++ iGCS Tests/iGCS_Tests.m
@@ -32,18 +32,6 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 - (void)testWaypointsHolderConversionEmpty {
     NSString* demoQGC = @"QGC WPL 110";
     
